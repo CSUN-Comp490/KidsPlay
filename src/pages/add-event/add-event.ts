498ddc14e1e6a248221906fe878f8ea2ac499483
tbyrd <terrelldbyrd@gmail.com>
2017-10-19 10:09:55 -0700

    try to commit

--- src/pages/add-event/add-event.ts
+++ src/pages/add-event/add-event.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { MyEventsPage } from '../my-events/my-events'
 
 /**
- * Generated class for the HomePage page.
+ * Generated class for the AddEventPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
@@ -10,16 +11,21 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html',
+  selector: 'page-add-event',
+  templateUrl: 'add-event.html',
 })
-export class HomePage {
+export class AddEventPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad HomePage');
+    console.log('ionViewDidLoad AddEventPage');
+  }
+
+  openMyEventsPage() {
+    this.navCtrl.setRoot(MyEventsPage, {}, {animate: true, direction: 'forward'});
   }
 
 }
