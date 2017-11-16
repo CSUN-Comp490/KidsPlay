6b0cc1c115791c3d502ab140c202ab29e974c304
Unknown <paulwilliamfussell@gmail.com>
2017-11-15 20:17:14 -0800

    Can now add kid value to an existing account in firebase

--- src/pages/registration/registration.ts
+++ src/pages/registration/registration.ts
@@ -2,6 +2,7 @@ import { AngularFireAuth } from 'angularfire2/auth';
 import { UserServiceProvider } from './../../providers/user-service/user-service';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {AuthProvider} from '../../providers/auth/auth';
 import * as firebase from 'firebase';
 
 /**
@@ -37,16 +38,17 @@ export class RegistrationPage {
   tparentEmail = '';
   tparentName = '';
   teenPassword = '';
+  teenEmail = '';
 
 
+  kids: string[][];
 
-
-
+  
 
 
   userProfile: any = firebase.database().ref('users');
   constructor(public navCtrl: NavController, public navParams: NavParams, public userService: UserServiceProvider,
-    public fireAuth: AngularFireAuth) {
+    public fireAuth: AngularFireAuth, public authservice: AuthProvider) {
   }
 
   ionViewDidLoad() {
@@ -71,9 +73,41 @@ export class RegistrationPage {
 
   // Adds a child to an existing parent
   createChild() {
+    // let   cred={
+    //   email: this.parentEmail,
+    //   password: this.parentPassword,
+    // } 
+
+  
+
+    //this.checkCredentials(cred);
+
+    this.fireAuth.auth.signInWithEmailAndPassword(this.parentEmail, this.parentPassword).then((authenticatedUser) => {
+      
+                this.userProfile.child(authenticatedUser.uid).update({
+
+                  kids: this.childfirstName,
+                })
+              })
 
   }
 
+  // Adds a teenager to an existing parent
+  createTeen() {
+    
+      }
 
 
+
+//   checkCredentials(credentials){
+
+//     this.authservice.login(credentials).then((res:any) =>{
+//        if(!res.code)
+//          return true;
+//        else 
+//        alert(res);
+//     })
+// }
+
+  
 }
