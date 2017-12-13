c30486caa9f24699a3e75ba3a467fb7b7b06ce9c
Unknown <edwinsepanosian@gmail.com>
2017-12-12 17:37:28 -0800

    Revert "Merge branch 'Edwin' into TrackBranch"
    This reverts commit 6bc7a1adcd0fe58ce3a2124709ba090184517cd4.

--- src/providers/user-service/user-service.ts
+++ src/providers/user-service/user-service.ts
@@ -64,6 +64,27 @@ signUpUser(email: string , password: string) {
         }
 
 //}
+getcurrentuid(){
+
+var promise = new Promise((resolve, reject)=> {
+this.firedata.equalTo('uid').once('value',(snapshot)=> {
+ let userdata= snapshot.val();
+ let temparr= [];
+ for (var key in userdata) {
+  temparr.push(userdata[key]);
+}
+resolve(temparr);
+})
+.catch((err)=>{
+  reject(err);
+})
+
+})
+return promise;
+
+}
+
+
 getallusers() {
     var promise = new Promise((resolve, reject) => {
       this.firedata.orderByChild('uid').once('value', (snapshot) => {
