70343cd07eba6a5a985a6830489173850e053427
Unknown <edwinsepanosian@gmail.com>
2018-02-21 09:59:49 -0800

    almost done with group messaging

--- src/pages/groupchatt/groupchatt.ts
+++ src/pages/groupchatt/groupchatt.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ActionSheetController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams, ActionSheetController,LoadingController,Content,Events } from 'ionic-angular';
 import {GroupsProvider} from '../../providers/groups/groups';
+import {ImghandlerProvider} from '../../providers/imghandler/imghandler';
 import {GroupChatPage } from '../group-chat/group-chat';
+import firebase from 'firebase';
 /**
  * Generated class for the GroupchattPage page.
  *
@@ -15,10 +17,18 @@ import {GroupChatPage } from '../group-chat/group-chat';
   templateUrl: 'groupchatt.html',
 })
 export class GroupchattPage {
+  @ViewChild('content') content:Content;
   owner: boolean = false;
   groupName;
+  newmessage;
+  allgroupmsgs;
+  alignuid;
+  photoURL;
+  imgornot;
   constructor(public navCtrl: NavController, public navParams: NavParams, public groupservice: GroupsProvider,
-              public actionSheet: ActionSheetController) {
+              public actionSheet: ActionSheetController, public events: Events, public imgstore: ImghandlerProvider,public loadingCtrl:LoadingController) {
+    this.alignuid = firebase.auth().currentUser.uid;
+    this.photoURL = firebase.auth().currentUser.photoURL;
     this.groupName = this.navParams.get('groupName');
     this.groupservice.getownership(this.groupName).then((res) => {
       if (res)
@@ -26,10 +36,54 @@ export class GroupchattPage {
     }).catch((err) => {
       alert(err);
       })
+      this.groupservice.getgroupmsgs(this.groupName);
+      this.events.subscribe('newgroupmsg', () => {
+        this.allgroupmsgs = [];
+        this.imgornot = [];
+        this.allgroupmsgs = this.groupservice.groupmsgs;
+        for (var key in this.allgroupmsgs) {
+          var d = new Date(this.allgroupmsgs[key].timestamp);
+          var hours = d.getHours();
+          var minutes = "0" + d.getMinutes();
+          var month = d.getMonth();
+          var da = d.getDate();
+  
+          var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+            "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+          
+          var formattedTime = monthNames[month] + "-" + da + "-" + hours + ":" + minutes.substr(-2);
+  
+          this.allgroupmsgs[key].timestamp = formattedTime;
+          if (this.allgroupmsgs[key].message.substring(0, 4) === 'http') {
+            this.imgornot.push(true);
+          }
+          else {
+            this.imgornot.push(false);
+          }
+        }
+        this.scrollto();
+      })
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad GroupchattPage');
+  }
+
+  sendpicmsg() {
+    let loader = this.loadingCtrl.create({
+      content: 'Please wait'
+    });
+    loader.present();
+    this.imgstore.picmsgstore().then((imgurl) => {
+      loader.dismiss();
+      this.groupservice.addgroupmsg(imgurl).then(() => {
+        this.scrollto();
+        this.newmessage = '';
+      })
+    }).catch((err) => {
+      alert(err);
+      loader.dismiss();
+    })
   }
 
   presentOwnerSheet() {
@@ -61,11 +115,11 @@ export class GroupchattPage {
           text: 'Delete Group',
           icon: 'trash',
           handler: () => {
-          //  this.groupservice.deletegroup().then(() => {
-            //  this.navCtrl.pop();
-           // }).catch((err) => {
-           //   console.log(err);
-           // })
+            this.groupservice.deletegroup().then(() => {
+              this.navCtrl.pop();
+            }).catch((err) => {
+              console.log(err);
+            })
           }
         },
         {
@@ -89,11 +143,11 @@ export class GroupchattPage {
           text: 'Leave Group',
           icon: 'log-out',
           handler: () => {
-            //this.groupservice.leavegroup().then(() => {
-             // this.navCtrl.pop();
-            //}).catch((err) => {
-            //  console.log(err);
-            //})
+            this.groupservice.leavegroup().then(() => {
+              this.navCtrl.pop();
+            }).catch((err) => {
+              console.log(err);
+            })
           }
         },
         {
@@ -116,4 +170,15 @@ export class GroupchattPage {
     sheet.present();
   }
 
+  addgroupmsg() {
+    this.groupservice.addgroupmsg(this.newmessage).then(() => {
+      this.scrollto();
+      this.newmessage = '';
+    })
+  }
+  scrollto() {
+    setTimeout(() => {
+      this.content.scrollToBottom();
+    }, 1000);
+  }
 }
--- src/pages/groupinfo/groupinfo.ts
+++ src/pages/groupinfo/groupinfo.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { IonicPage, NavController, NavParams, Events } from 'ionic-angular';
+import { GroupsProvider } from '../../providers/groups/groups';
 /**
  * Generated class for the GroupinfoPage page.
  *
@@ -15,11 +15,28 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class GroupinfoPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  groupmembers;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public groupservice: GroupsProvider,
+              public events: Events) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad GroupinfoPage');
+    this.groupservice.getownership(this.groupservice.currentgroupname).then((res) => {
+      if (res)
+        this.groupmembers = this.groupservice.currentgroup;
+      else {
+        this.groupservice.getgroupmembers();
+      }
+        
+    })
+
+    this.events.subscribe('gotmembers', () => {
+      this.groupmembers = this.groupservice.currentgroup;
+    }) 
+  }
+
+  ionViewWillLeave() {
+    this.events.unsubscribe('gotmembers');
   }
 
 }
